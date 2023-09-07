ed96a850a1300ef0eb6d10df9f4fbd7b4c6c7b65
Joaquin Espeche <Joaquinespeche491@gmail.com>
2023-09-06 21:35:16 -0300

    1.2

--- js/documento.js
+++ js/documento.js
@@ -1,3 +1,4 @@
+
 function mostrarFechaHoraActual() {
   var fechaHoraActual = new Date();
   var fechaActual = fechaHoraActual.toISOString().slice(0, 10);
@@ -12,6 +13,23 @@ function mostrarFechaHoraActual() {
 window.onload = function () {
   mostrarFechaHoraActual();
 };
+
+var toggleButton = document.getElementById('toggleFullScreen');
+
+toggleButton.addEventListener('click', function() {
+    var element = document.documentElement; // Elemento raíz de la página (el body o el html)
+    
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { // Para navegadores Firefox
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { // Para navegadores WebKit (Chrome, Safari)
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { // Para Internet Explorer
+        element.msRequestFullscreen();
+    }
+});
+
 
 document.addEventListener('DOMContentLoaded', function() {
   const datosGuardados = localStorage.getItem('datosUsuario');
